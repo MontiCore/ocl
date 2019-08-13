39ea1c0ef213a34d381de2fa7b1b1d12a32b381f
Bernhard Rumpe <rumpe@se-rwth.de>
2019-08-13 10:41:45 +0200

    BR
    Former-commit-id: 1372541de1b61f8f288619e00092c4a24654f680
    Former-commit-id: 6b71e7aa28833900ad695da6e759d67aca586742

--- OCLCDTool/exampleWithTypes.bat
+++ OCLCDTool/exampleWithTypes.bat
@@ -1,3 +1,4 @@
+@rem (c) https://github.com/MontiCore/monticore  
 @REM
 @REM ******************************************************************************
 @REM  MontiCAR Modeling Family, www.se-rwth.de
