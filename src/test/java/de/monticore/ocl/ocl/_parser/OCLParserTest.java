9ebbc71482993bb8e0201d1791148d485e593997
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2020-11-03 17:07:38 +0100

    Add (deactivated) parser test
    Former-commit-id: a9922db049472dd7ced6b6fa57df5b1b09700b9a
    Former-commit-id: 5d9239e5afcae0b34e81df13b319850cd8471c03

---
+++ src/test/java/de/monticore/ocl/ocl/_parser/OCLParserTest.java
@@ -0,0 +1,59 @@
+package de.monticore.ocl.ocl._parser;
+
+import de.monticore.ocl.ocl._ast.ASTOCLCompilationUnit;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OCLParserTest {
+
+  protected static final String RELATIVE_MODEL_PATH = "src/test/resources";
+
+  public Optional<ASTOCLCompilationUnit> parse(String relativeFilePath,
+    boolean expParserErrors) {
+    OCLParser parser = new OCLParser();
+    Optional<ASTOCLCompilationUnit> optAst;
+    try {
+      optAst = parser.parse(relativeFilePath);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    if (expParserErrors) {
+      assertThat(parser.hasErrors()).isTrue();
+      assertThat(optAst).isNotPresent();
+    } else {
+      assertThat(parser.hasErrors()).isFalse();
+      assertThat(optAst).isPresent();
+    }
+    return optAst;
+  }
+
+  /*
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "example/cocos/valid/validConstructorName.ocl",
+    "example/cocos/valid/validConstructorName0.ocl",
+    "example/cocos/valid/validFileName.ocl",
+    "example/cocos/valid/validInvariantName.ocl",
+    "example/cocos/valid/validMethodDeclarationName.ocl",
+    "example/cocos/valid/validMethSigName.ocl",
+    "example/cocos/valid/validParameterDeclarationName.ocl",
+    "example/cocos/valid/validParameterType.ocl",
+    "example/cocos/valid/validPostStatementName.ocl",
+    "example/cocos/valid/validPrePost.ocl",
+    "example/cocos/valid/validPreStatementName.ocl",
+    "example/cocos/valid/validTypes.ocl",
+    "example/cocos/valid/validVariableName.ocl"})
+   */
+  public void shouldParseValidInput(String fileName) {
+    this.parse(Paths.get(RELATIVE_MODEL_PATH, fileName).toString(), false);
+  }
+
+
+
+}
