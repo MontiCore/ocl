fdfc522f2ed8fa1633c1092e39d697b808a00b05
Anno Kleiss <anno.kleiss@rwth-aachen.de>
2020-11-24 17:33:44 +0100

    add visit methods for most nonterminals in OCLExpressions typecheck

--- src/main/java/de/monticore/ocl/types/check/DeriveSymTypeOfOCLExpressions.java
+++ src/main/java/de/monticore/ocl/types/check/DeriveSymTypeOfOCLExpressions.java
@@ -1,7 +1,12 @@
 package de.monticore.ocl.types.check;
 
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.ocl.expressions.oclexpressions._ast.*;
 import de.monticore.ocl.expressions.oclexpressions._visitor.OCLExpressionsVisitor;
-import de.monticore.types.check.DeriveSymTypeOfExpression;
+import de.monticore.types.check.*;
+import de.se_rwth.commons.logging.Log;
+
+import java.util.Optional;
 
 public class DeriveSymTypeOfOCLExpressions extends DeriveSymTypeOfExpression implements OCLExpressionsVisitor {
 
@@ -19,5 +24,446 @@ public class DeriveSymTypeOfOCLExpressions extends DeriveSymTypeOfExpression imp
   @Override
   public OCLExpressionsVisitor getRealThis() {
     return realThis;
+  }
+
+  @Override
+  public void traverse(ASTTypeCastExpression node) {
+    SymTypeExpression exprResult = null;
+    SymTypeExpression typeResult = null;
+
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      exprResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3080 The type of the expression of the OCLTypeCastExpression could not be calculated");
+      return;
+    }
+
+    if (node.getMCType() != null) {
+      node.getMCType().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      typeResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3081 The type of the MCType of the OCLTypeCastExpression could not be calculated");
+      return;
+    }
+
+    if (!TypeCheck.compatible(typeResult, exprResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3082 The type of the expression of the OCLTypeCastExpression can't be cast to given type");
+      return;
+    }
+    else {
+      typeCheckResult.setCurrentResult(typeResult);
+    }
+  }
+
+  @Override
+  public void traverse(ASTTypeIfExpression node) {
+    SymTypeExpression thenResult = null;
+    SymTypeExpression elseResult = null;
+
+    if (node.getMCType() != null) {
+      node.getMCType().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3012 The type of the MCType of the OCLInstanceOfExpression of the OCLTypeIfExpr could not be calculated");
+      return;
+    }
+
+    if (node.getThenExpression() != null) {
+      node.getThenExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      thenResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3013 The type of the then expression of the OCLTypeIfExpression could not be calculated");
+      return;
+    }
+
+    if (node.getElseExpression() != null) {
+      node.getElseExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      elseResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3014 The type of the else expression of the OCLTypeIfExpr could not be calculated");
+      return;
+    }
+
+    if (TypeCheck.compatible(thenResult, elseResult)) {
+      typeCheckResult.setCurrentResult(thenResult);
+    }
+    else if (TypeCheck.isSubtypeOf(thenResult, elseResult)) {
+      typeCheckResult.setCurrentResult(elseResult);
+    }
+    else {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3015 The type of the else expression of the OCLTypeIfExpr doesn't match the then expression");
+      return;
+    }
+  }
+
+  @Override
+  public void traverse(ASTIfThenElseExpression node) {
+    SymTypeExpression conditionResult = null;
+    SymTypeExpression thenResult = null;
+    SymTypeExpression elseResult = null;
+
+    if (node.getCondition() != null) {
+      node.getCondition().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      conditionResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3040 The type of the left expression of the OCLIfThenElseExpr could not be calculated");
+      return;
+    }
+
+    // the condition has to be boolean
+    if (!TypeCheck.isBoolean(conditionResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3041 The type of the condition of the OCLIfThenElseExpr has to be boolean");
+      return;
+    }
+
+    if (node.getThenExpression() != null) {
+      node.getThenExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      thenResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3042 The type of the then expression of the OCLIfThenElseExpr could not be calculated");
+    }
+
+    if (node.getElseExpression() != null) {
+      node.getElseExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      elseResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3043 The type of the else expression of the OCLIfThenElseExpr could not be calculated");
+      return;
+    }
+
+    if (TypeCheck.compatible(thenResult, elseResult)) {
+      // Type of else is subtype of/or same type as then -> return then-type
+      typeCheckResult.setCurrentResult(thenResult);
+    }
+    else if (TypeCheck.isSubtypeOf(thenResult, elseResult)) {
+      // Type of then is subtype of else -> return else-type
+      typeCheckResult.setCurrentResult(elseResult);
+    }
+    else {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3044 The type of the else expression of the OCLIfThenElseExpr doesn't match the then expression");
+      return;
+    }
+  }
+
+  @Override
+  public void traverse(ASTImpliesExpression node) {
+    // sets the last result
+    checkAndSetBooleanTypes(node.getLeft(), node.getRight(), "ImpliesExpression");
+  }
+
+  @Override
+  public void traverse(ASTEquivalentExpression node) {
+    // sets the last result
+    checkAndSetBooleanTypes(node.getLeft(), node.getRight(), "EquivalentExpression");
+  }
+
+  @Override
+  public void traverse(ASTForallExpression node) {
+    SymTypeExpression exprResult = null;
+    
+    if(node.getExpression() != null){
+      node.getExpression().accept(getRealThis());
+    }
+    if(typeCheckResult.isPresentCurrentResult()){
+      exprResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else{
+      Log.error("0xA3211 The type of the expression in the ForallExpression could not be calculated");
+    }
+    if (!TypeCheck.isBoolean(exprResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3212 The type of the expression in the ForallExpression has to be boolean");
+      return;
+    }
+    else {
+      typeCheckResult.setCurrentResult(createBoolean());
+    }
+  }
+
+  @Override
+  public void traverse(ASTExistsExpression node) {
+    SymTypeExpression exprResult = null;
+
+    if(node.getExpression() != null){
+      node.getExpression().accept(getRealThis());
+    }
+    if(typeCheckResult.isPresentCurrentResult()){
+      exprResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else{
+      Log.error("0xA3211 The type of the expression in the ExistsExpression could not be calculated");
+    }
+    if (!TypeCheck.isBoolean(exprResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3212 The type of the expression in the ExistsExpression has to be boolean");
+      return;
+    }
+    else {
+      typeCheckResult.setCurrentResult(createBoolean());
+    }
+  }
+
+  @Override
+  public void traverse(ASTAnyExpression node) {
+    SymTypeExpression exprResult = null;
+
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      exprResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3050 The type of the expression of the OCLAnyExpr could not be calculated");
+      return;
+    }
+
+    if (exprResult instanceof SymTypeOfGenerics) {
+      typeCheckResult.setCurrentResult(exprResult);
+    }
+    else {
+      typeCheckResult.setCurrentResult(exprResult);
+    }
+  }
+
+  @Override
+  public void traverse(ASTLetinExpression node) {
+    SymTypeExpression exprResult = null;
+
+    if (node.getOCLVariableDeclarationList() != null && !node.getOCLVariableDeclarationList().isEmpty()) {
+      for (ASTOCLVariableDeclaration dec : node.getOCLVariableDeclarationList()) {
+        dec.accept(getRealThis());
+        if (!typeCheckResult.isPresentCurrentResult()) {
+          Log.error("0xA3060 The type of the OCLVariableDeclaration of the LetinExpr could not be calculated");
+          return;
+        }
+      }
+    }
+
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      exprResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3061 The type of the expression of the LetinExpr could not be calculated");
+      return;
+    }
+
+    typeCheckResult.setCurrentResult(exprResult);
+  }
+
+  @Override
+  public void traverse(ASTIterateExpression node) {
+    SymTypeExpression valueResult = null;
+    SymTypeExpression initResult = null;
+
+    if (node.getInit() != null) {
+      node.getInit().accept(getRealThis());
+    }
+    if (!typeCheckResult.isPresentCurrentResult()) {
+      Log.error("0xA3071 The type of the init of the OCLIterateExpression could not be calculated");
+      return;
+    }
+    else {
+      initResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+
+    if (node.getValue() != null) {
+      node.getValue().accept(getRealThis());
+    }
+    if (!typeCheckResult.isPresentCurrentResult()) {
+      Log.error("0xA3073 The type of the value of the OCLIterateExpression could not be calculated");
+      return;
+    }
+    else {
+      valueResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+
+    if (!TypeCheck.compatible(initResult, valueResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3074 The type of the value of the OCLIterateExpression (" + valueResult.print() +
+              ") has to match the type of the init declaration (" + initResult.print() + ")");
+      return;
+    }
+    else {
+      typeCheckResult.setCurrentResult(initResult);
+    }
+  }
+  
+  //TODO: InDeclaration, InDeclarationVariable
+
+  @Override
+  public void traverse(ASTInstanceOfExpression node) {
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3000 The type of the left expression of the OCLInstanceOfExpression could not be calculated");
+      return;
+    }
+
+    if (node.getMCType() != null) {
+      node.getMCType().accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3001 The type of the OCLExtType of the OCLInstanceOfExpression could not be calculated");
+      return;
+    }
+
+    final SymTypeExpression wholeResult = createBoolean();
+    typeCheckResult.setCurrentResult(wholeResult);
+  }
+
+  @Override
+  public void traverse(ASTOCLArrayQualification node){
+    SymTypeExpression exprResult;
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (!typeCheckResult.isPresentCurrentResult()) {
+      Log.error("0xA3001 The type of the expression of the OCLArrayQualification could not be calculated");
+      return;
+    }
+    exprResult = typeCheckResult.getCurrentResult();
+    typeCheckResult.setCurrentResultAbsent();
+    for (ASTExpression e : node.getArgumentsList()){
+      if (e != null) {
+        e.accept(getRealThis());
+      }
+      if (!typeCheckResult.isPresentCurrentResult()) {
+        Log.error("0xA3001 The type of a expression in the arguments of the OCLArrayQualification could not be calculated");
+        return;
+      }
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    typeCheckResult.setCurrentResult(exprResult);
+  }
+
+  @Override
+  public void traverse(ASTOCLAtPreQualification node){
+    SymTypeExpression exprResult;
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (!typeCheckResult.isPresentCurrentResult()) {
+      Log.error("0xA3001 The type of the expression of the OCLAtPreQualification could not be calculated");
+      return;
+    }
+    exprResult = typeCheckResult.getCurrentResult();
+    typeCheckResult.setCurrentResultAbsent();
+    typeCheckResult.setCurrentResult(exprResult);
+  }
+
+  @Override
+  public void traverse(ASTOCLTransitiveQualification node){
+    SymTypeExpression exprResult;
+    if (node.getExpression() != null) {
+      node.getExpression().accept(getRealThis());
+    }
+    if (!typeCheckResult.isPresentCurrentResult()) {
+      Log.error("0xA3001 The type of the expression of the OCLTransitiveQualification could not be calculated");
+      return;
+    }
+    exprResult = typeCheckResult.getCurrentResult();
+    typeCheckResult.setCurrentResultAbsent();
+    typeCheckResult.setCurrentResult(exprResult);
+  }
+  
+  private void checkAndSetBooleanTypes(ASTExpression left, ASTExpression right, String astType) {
+    SymTypeExpression leftResult;
+    SymTypeExpression rightResult;
+
+    if (left != null) {
+      left.accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      leftResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3200 The type of the left expression of the " + astType + " could not be calculated");
+      return;
+    }
+
+    if (!TypeCheck.isBoolean(leftResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3201 The type of the left expression of the " + astType + " has to be boolean");
+      return;
+    }
+
+    if (right != null) {
+      right.accept(getRealThis());
+    }
+    if (typeCheckResult.isPresentCurrentResult()) {
+      rightResult = typeCheckResult.getCurrentResult();
+      typeCheckResult.setCurrentResultAbsent();
+    }
+    else {
+      Log.error("0xA3202 The type of the right expression of the " + astType + " could not be calculated");
+      return;
+    }
+
+    if (!TypeCheck.isBoolean(rightResult)) {
+      typeCheckResult.setCurrentResultAbsent();
+      Log.error("0xA3203 The type of the right expression of the " + astType + " has to be boolean");
+      return;
+    }
+
+    // return type is always boolean
+    typeCheckResult.setCurrentResult(createBoolean());
+  }
+
+  public static SymTypeExpression createBoolean() {
+    return SymTypeExpressionFactory.createTypeConstant("boolean");
   }
 }
