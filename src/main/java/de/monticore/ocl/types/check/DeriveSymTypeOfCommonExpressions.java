ad162c0f979ff59d84bee36a3e4623005da50d8a
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2021-01-28 19:16:09 +0100

    Add CD4A association workaround
    TODO: Remove once CD4A typechecks are fixed

--- src/main/java/de/monticore/ocl/types/check/DeriveSymTypeOfCommonExpressions.java
+++ src/main/java/de/monticore/ocl/types/check/DeriveSymTypeOfCommonExpressions.java
@@ -1,5 +1,7 @@
 package de.monticore.ocl.types.check;
 
+import de.monticore.cdassociation._ast.ASTCDRole;
+import de.monticore.cdassociation._symboltable.CDRoleSymbol;
 import de.monticore.expressions.commonexpressions._ast.ASTCallExpression;
 import de.monticore.expressions.commonexpressions._ast.ASTConditionalExpression;
 import de.monticore.expressions.commonexpressions._ast.ASTFieldAccessExpression;
@@ -21,7 +23,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static de.monticore.ocl.types.check.OCLTypeCheck.*;
+import static de.monticore.ocl.types.check.OCLTypeCheck.compatible;
+import static de.monticore.ocl.types.check.OCLTypeCheck.isBoolean;
 
 public class DeriveSymTypeOfCommonExpressions
   extends de.monticore.types.check.DeriveSymTypeOfCommonExpressions {
@@ -33,10 +36,10 @@ public class DeriveSymTypeOfCommonExpressions
    * used instead of the normal TypeCheck methods.
    */
 
-
   @Override
-  public void traverse(ASTFieldAccessExpression expr){
-    CommonExpressionsFullPrettyPrinter printer = new CommonExpressionsFullPrettyPrinter(new IndentPrinter());
+  public void traverse(ASTFieldAccessExpression expr) {
+    CommonExpressionsFullPrettyPrinter printer = new CommonExpressionsFullPrettyPrinter(
+      new IndentPrinter());
     SymTypeExpression innerResult;
     expr.getExpression().accept(getTraverser());
     if (typeCheckResult.isPresentCurrentResult()) {
@@ -45,59 +48,85 @@ public class DeriveSymTypeOfCommonExpressions
       //look for this type in our scope
       TypeSymbol innerResultType = innerResult.getTypeInfo();
       //search for a method, field or type in the scope of the type of the inner expression
-      List<VariableSymbol> fieldSymbols = innerResult.getFieldList(expr.getName(), typeCheckResult.isType());
-      Optional<TypeSymbol> typeSymbolOpt = innerResultType.getSpannedScope().resolveType(expr.getName());
+      List<VariableSymbol> fieldSymbols = innerResult
+        .getFieldList(expr.getName(), typeCheckResult.isType());
+      Optional<TypeSymbol> typeSymbolOpt = innerResultType.getSpannedScope()
+        .resolveType(expr.getName());
       if (!fieldSymbols.isEmpty()) {
         //cannot be a method, test variable first
         //durch AST-Umbau kann ASTFieldAccessExpression keine Methode sein
         //if the last result is a type then filter for static field symbols
-        if(typeCheckResult.isType()){
+        if (typeCheckResult.isType()) {
           fieldSymbols = filterStaticFieldSymbols(fieldSymbols);
         }
         if (fieldSymbols.size() != 1) {
-          if(!checkIfCorrect(fieldSymbols)){
+          if (!checkIfCorrect(fieldSymbols)) {
             typeCheckResult.reset();
             logError("0xA0237", expr.get_SourcePositionStart());
           }
         }
-        if(!fieldSymbols.isEmpty()) {
+        if (!fieldSymbols.isEmpty()) {
           VariableSymbol var = fieldSymbols.get(0);
           SymTypeExpression type = var.getType();
+
+          //TODO: Fixt dass CD4A keine Arraytypen für Assiziationen setzt
+          if (var.getAstNode() instanceof ASTCDRole) {
+            ASTCDRole astRole = (ASTCDRole) var.getAstNode();
+            CDRoleSymbol symbol = astRole.getSymbol();
+            if (symbol.isPresentCardinality()) {
+              if (symbol.getCardinality().isMult() ||
+                symbol.getCardinality().isAtLeastOne()) {
+                type = SymTypeExpressionFactory
+                  .createTypeArray(type.getTypeInfo(), symbol.getCardinality().getUpperBound(),
+                    type);
+              }
+            }
+          }
+
           typeCheckResult.setField();
           typeCheckResult.setCurrentResult(type);
         }
-      } else if (typeSymbolOpt.isPresent()) {
+      }
+      else if (typeSymbolOpt.isPresent()) {
         //no variable found, test type
         TypeSymbol typeSymbol = typeSymbolOpt.get();
         boolean match = true;
         //if the last result is a type and the type is not static then it is not accessible
-        if(typeCheckResult.isType()){
-          if(!(typeSymbol instanceof OOTypeSymbol) || !(((OOTypeSymbol) typeSymbol).isIsStatic())) {
+        if (typeCheckResult.isType()) {
+          if (!(typeSymbol instanceof OOTypeSymbol) || !(((OOTypeSymbol) typeSymbol)
+            .isIsStatic())) {
             match = false;
           }
         }
-        if(match){
-          SymTypeExpression wholeResult = SymTypeExpressionFactory.createTypeExpression(typeSymbol.getName(), typeSymbol.getEnclosingScope());
+        if (match) {
+          SymTypeExpression wholeResult = SymTypeExpressionFactory
+            .createTypeExpression(typeSymbol.getName(), typeSymbol.getEnclosingScope());
           typeCheckResult.setType();
           typeCheckResult.setCurrentResult(wholeResult);
-        }else{
+        }
+        else {
           typeCheckResult.reset();
           logError("0xA0303", expr.get_SourcePositionStart());
         }
-      }else{
+      }
+      else {
         typeCheckResult.reset();
         logError("0xA0306", expr.get_SourcePositionStart());
       }
-    } else {
+    }
+    else {
       //inner type has no result --> try to resolve a type
       String toResolve = printer.prettyprint(expr);
-      Optional<TypeSymbol> typeSymbolOpt = getScope(expr.getEnclosingScope()).resolveType(toResolve);
+      Optional<TypeSymbol> typeSymbolOpt = getScope(expr.getEnclosingScope())
+        .resolveType(toResolve);
       if (typeSymbolOpt.isPresent()) {
         TypeSymbol typeSymbol = typeSymbolOpt.get();
-        SymTypeExpression type = SymTypeExpressionFactory.createTypeExpression(typeSymbol.getName(), typeSymbol.getEnclosingScope());
+        SymTypeExpression type = SymTypeExpressionFactory
+          .createTypeExpression(typeSymbol.getName(), typeSymbol.getEnclosingScope());
         typeCheckResult.setType();
         typeCheckResult.setCurrentResult(type);
-      } else {
+      }
+      else {
         //the inner type has no result and there is no type found
         typeCheckResult.reset();
         Log.info("package suspected", "DeriveSymTypeOfCommonExpressions");
@@ -106,23 +135,24 @@ public class DeriveSymTypeOfCommonExpressions
   }
 
   protected List<VariableSymbol> filterStaticFieldSymbols(List<VariableSymbol> fieldSymbols) {
-    return fieldSymbols.stream().filter(f -> f instanceof FieldSymbol).filter(f -> ((FieldSymbol) f).isIsStatic()).collect(Collectors.toList());
+    return fieldSymbols.stream().filter(f -> f instanceof FieldSymbol)
+      .filter(f -> ((FieldSymbol) f).isIsStatic()).collect(Collectors.toList());
   }
 
-  protected boolean checkIfCorrect(List<VariableSymbol> fieldSymbols){
-    if(fieldSymbols.size()==0){
+  protected boolean checkIfCorrect(List<VariableSymbol> fieldSymbols) {
+    if (fieldSymbols.size() == 0) {
       return false;
     }
     TypeSymbol type = fieldSymbols.get(0).getType().getTypeInfo();
-    if(type instanceof TypeSymbolSurrogate){
+    if (type instanceof TypeSymbolSurrogate) {
       type = ((TypeSymbolSurrogate) type).lazyLoadDelegate();
     }
-    for(VariableSymbol field: fieldSymbols){
+    for (VariableSymbol field : fieldSymbols) {
       TypeSymbol typeOfField = field.getType().getTypeInfo();
-      if(typeOfField instanceof TypeSymbolSurrogate){
+      if (typeOfField instanceof TypeSymbolSurrogate) {
         typeOfField = ((TypeSymbolSurrogate) typeOfField).lazyLoadDelegate();
       }
-      if(!type.equals(typeOfField)){
+      if (!type.equals(typeOfField)) {
         return false;
       }
     }
@@ -130,19 +160,22 @@ public class DeriveSymTypeOfCommonExpressions
   }
 
   @Override
-  protected Optional<SymTypeExpression> calculateConditionalExpressionType(ASTConditionalExpression expr,
-                                                                           SymTypeExpression conditionResult,
-                                                                           SymTypeExpression trueResult,
-                                                                           SymTypeExpression falseResult) {
+  protected Optional<SymTypeExpression> calculateConditionalExpressionType
+    (ASTConditionalExpression expr,
+      SymTypeExpression conditionResult,
+      SymTypeExpression trueResult,
+      SymTypeExpression falseResult) {
     Optional<SymTypeExpression> wholeResult = Optional.empty();
     //condition has to be boolean
     if (isBoolean(conditionResult)) {
       //check if "then" and "else" are either from the same type or are in sub-supertype relation
       if (compatible(trueResult, falseResult)) {
         wholeResult = Optional.of(trueResult);
-      } else if (compatible(falseResult, trueResult)) {
+      }
+      else if (compatible(falseResult, trueResult)) {
         wholeResult = Optional.of(falseResult);
-      } else {
+      }
+      else {
         // first argument can be null since it should not be relevant to the type calculation
         wholeResult = getBinaryNumericPromotion(trueResult, falseResult);
       }
@@ -151,15 +184,16 @@ public class DeriveSymTypeOfCommonExpressions
   }
 
   @Override
-  protected Optional<SymTypeExpression> calculateTypeLogical(ASTInfixExpression expr, SymTypeExpression rightResult, SymTypeExpression leftResult) {
+  protected Optional<SymTypeExpression> calculateTypeLogical(ASTInfixExpression
+    expr, SymTypeExpression rightResult, SymTypeExpression leftResult) {
     //Option one: they are both numeric types
     if (isNumericType(leftResult) && isNumericType(rightResult)
-            || isBoolean(leftResult) && isBoolean(rightResult)) {
+      || isBoolean(leftResult) && isBoolean(rightResult)) {
       return Optional.of(SymTypeExpressionFactory.createTypeConstant("boolean"));
     }
     //Option two: none of them is a primitive type and they are either the same type or in a super/sub type relation
     if (!leftResult.isTypeConstant() && !rightResult.isTypeConstant() &&
-            (compatible(leftResult, rightResult) || compatible(rightResult, leftResult))
+      (compatible(leftResult, rightResult) || compatible(rightResult, leftResult))
     ) {
       return Optional.of(SymTypeExpressionFactory.createTypeConstant("boolean"));
     }
@@ -167,7 +201,8 @@ public class DeriveSymTypeOfCommonExpressions
     return Optional.empty();
   }
 
-  private List<FunctionSymbol> getFittingMethods(List<FunctionSymbol> methodlist, ASTCallExpression expr){
+  private List<FunctionSymbol> getFittingMethods
+    (List<FunctionSymbol> methodlist, ASTCallExpression expr) {
     List<FunctionSymbol> fittingMethods = new ArrayList<>();
     for (FunctionSymbol method : methodlist) {
       //for every method found check if the arguments are correct
@@ -176,8 +211,10 @@ public class DeriveSymTypeOfCommonExpressions
         for (int i = 0; i < method.getParameterList().size(); i++) {
           expr.getArguments().getExpression(i).accept(getTraverser());
           //test if every single argument is correct
-          if (!method.getParameterList().get(i).getType().deepEquals(typeCheckResult.getCurrentResult()) &&
-                  !compatible(method.getParameterList().get(i).getType(), typeCheckResult.getCurrentResult())) {
+          if (!method.getParameterList().get(i).getType()
+            .deepEquals(typeCheckResult.getCurrentResult()) &&
+            !compatible(method.getParameterList().get(i).getType(),
+              typeCheckResult.getCurrentResult())) {
             success = false;
           }
         }
