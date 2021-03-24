3a49bc52385d7aa66277478eda548cf723252a97
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2021-03-24 11:05:47 +0100

    Remove commented out code

--- src/main/java/de/monticore/ocl/ocl/_symboltable/OCLGlobalScope.java
+++ src/main/java/de/monticore/ocl/ocl/_symboltable/OCLGlobalScope.java
@@ -3,9 +3,6 @@ package de.monticore.ocl.ocl._symboltable;
 
 import de.monticore.ocl.ocl.OCLMill;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
-import de.monticore.symbols.basicsymbols._symboltable.FunctionSymbolDeSer;
-import de.monticore.symbols.basicsymbols._symboltable.TypeSymbolDeSer;
-import de.monticore.symbols.basicsymbols._symboltable.VariableSymbolDeSer;
 
 public class OCLGlobalScope extends OCLGlobalScopeTOP {
   @Override public OCLGlobalScope getRealThis() {
@@ -15,13 +12,6 @@ public class OCLGlobalScope extends OCLGlobalScopeTOP {
   @Override
   public void init() {
     super.init();
-    /*
-    this.putSymbolDeSer("de.monticore.cdbasis._symboltable.CDTypeSymbol", new TypeSymbolDeSer());
-    this.putSymbolDeSer("de.monticore.cd4codebasis._symboltable.CDMethodSignatureSymbol",
-      new FunctionSymbolDeSer());
-    this.putSymbolDeSer("de.monticore.symbols.oosymbols._symboltable.FieldSymbol",
-      new VariableSymbolDeSer());
-    */
     add(OCLMill.typeSymbolBuilder()
       .setName(BasicSymbolsMill.NULL)
       .setEnclosingScope(this)
