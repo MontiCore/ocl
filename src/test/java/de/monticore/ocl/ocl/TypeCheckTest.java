31ab9a401639b4e671889a5e69e060e72d57c3d3
conBuschhaus <constantin.buschhaus@rwth-aachen.de>
2021-10-21 14:37:18 +0200

    #00 probably a bug in monticore typecheck

---
+++ src/test/java/de/monticore/ocl/ocl/TypeCheckTest.java
@@ -0,0 +1,42 @@
+package de.monticore.ocl.ocl;
+
+import de.monticore.ocl.ocl._ast.ASTOCLCompilationUnit;
+import de.monticore.ocl.ocl._ast.ASTOCLInvariant;
+import de.monticore.ocl.types.check.DeriveSymTypeOfOCLCombineExpressions;
+import de.monticore.ocl.types.check.FullSynthesizeSymTypeFromMCSimpleGenericTypes;
+import de.monticore.ocl.util.SymbolTableUtil;
+import de.monticore.types.check.TypeCheck;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TypeCheckTest extends AbstractTest {
+
+  @Test
+  public void shouldCreateSymTabForValidModels() throws IOException {
+    String filename = "list.ocl";
+
+    // given
+    final Optional<ASTOCLCompilationUnit> ast = parse(prefixValidModelsPath(filename), false);
+    assertThat(ast).isPresent();
+
+    SymbolTableUtil.prepareMill();
+    SymbolTableUtil.addCd4cSymbols();
+    SymbolTableUtil.loadSymbolFile("src/test/resources/testinput/CDs/AuctionCD.sym");
+    SymbolTableUtil.loadSymbolFile("src/test/resources/testinput/CDs/DefaultTypes.sym");
+
+    // when / then
+    SymbolTableUtil.runSymTabGenitor(ast.get());
+    SymbolTableUtil.runSymTabCompleter(ast.get());
+
+    TypeCheck typeCheck = new TypeCheck(new FullSynthesizeSymTypeFromMCSimpleGenericTypes(),
+        new DeriveSymTypeOfOCLCombineExpressions());
+
+    System.out.println(typeCheck.typeOf(((ASTOCLInvariant) ast.get().getOCLArtifact().getOCLConstraint(0)).getExpression()));
+  }
+}
