088182e474438e9b1269bfe8572e2882d260bb16
conBuschhaus <constantin.buschhaus@rwth-aachen.de>
2021-10-22 12:02:25 +0200

    #00 clean up

--- src/test/java/de/monticore/ocl/ocl/TypeCheckTest.java
+++ src/test/java/de/monticore/ocl/ocl/TypeCheckTest.java
@@ -5,27 +5,29 @@ import de.monticore.ocl.ocl._ast.ASTOCLInvariant;
 import de.monticore.ocl.types.check.DeriveSymTypeOfOCLCombineExpressions;
 import de.monticore.ocl.types.check.FullSynthesizeSymTypeFromMCSimpleGenericTypes;
 import de.monticore.ocl.util.SymbolTableUtil;
+import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.TypeCheck;
 import de.se_rwth.commons.logging.Log;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TypeCheckTest extends AbstractTest {
 
-  //@Disabled
+  @Disabled
   @Test
-  public void testTypCheckForGenericMethodCalls() throws IOException {
+  public void testTypCheckForGenericMethodCalls(String arg) {
     String filename = "list.ocl";
 
     Log.enableFailQuick(false);
 
     // given
-    final Optional<ASTOCLCompilationUnit> ast = parse(prefixValidModelsPath(filename), false);
+    final Optional<ASTOCLCompilationUnit> ast =
+        parse(RELATIVE_MODEL_PATH + "/testinput"+"/typeInferringModels/" +filename,
+        false);
     assertThat(ast).isPresent();
 
     SymbolTableUtil.prepareMill();
@@ -42,9 +44,14 @@ public class TypeCheckTest extends AbstractTest {
 
     // NullpointerException, weil TypeCheck eigentlich schon vorher mit Log.error abbrechen sollte
     try {
-      typeCheck.typeOf(((ASTOCLInvariant) ast.get().getOCLArtifact().getOCLConstraint(0)).getExpression());
-    } catch(NullPointerException e) {}
+      SymTypeExpression t =
+          typeCheck.typeOf(((ASTOCLInvariant) ast.get().getOCLArtifact().getOCLConstraint(0)).getExpression());
+    } catch(NullPointerException e) {
+      Log.error("This should not happen. The model was valid. All types were loaded. What "
+          + "happened?");
+    }
 
+    // Additional check that nothing broke
     assertThat(Log.getErrorCount()).isEqualTo(0);
   }
 }
