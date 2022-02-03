2959d50bb6299c90ec16398368f541a4e700ef83
David Schmalzing <schmalzing@se-rwth.de>
2022-02-03 18:25:43 +0100

    Add expression end visits

--- src/main/java/de/monticore/ocl/codegen/visitors/CommonExpressionsPrinter.java
+++ src/main/java/de/monticore/ocl/codegen/visitors/CommonExpressionsPrinter.java
@@ -7,6 +7,7 @@ import de.monticore.expressions.commonexpressions._visitor.CommonExpressionsHand
 import de.monticore.expressions.commonexpressions._visitor.CommonExpressionsTraverser;
 import de.monticore.expressions.commonexpressions._visitor.CommonExpressionsVisitor2;
 import de.monticore.ocl.codegen.util.VariableNaming;
+import de.monticore.ocl.oclexpressions._ast.ASTEquivalentExpression;
 
 public class CommonExpressionsPrinter extends AbstractPrinter implements CommonExpressionsHandler, CommonExpressionsVisitor2 {
 
@@ -51,7 +52,118 @@ public class CommonExpressionsPrinter extends AbstractPrinter implements CommonE
   @Override
   public void endVisit(ASTMultExpression node) {
     Preconditions.checkNotNull(node);
-    handleInfixExpression(node, "*", "times");
+    this.handleInfixExpression(node, "*", "times");
+  }
+
+  @Override
+  public void endVisit(ASTDivideExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "/", "divide");
+  }
+
+  @Override
+  public void endVisit(ASTModuloExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "%");
+  }
+
+  @Override
+  public void endVisit(ASTPlusExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "+", "plus");
+  }
+
+  @Override
+  public void endVisit(ASTMinusExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "-", "minus");
+  }
+
+  //TODO
+  public void endVisit(ASTEquivalentExpression node) {
+    Preconditions.checkNotNull(node);
+    this.getStringBuilder().append("Boolean ")
+      .append(this.getNaming().getName(node))
+      .append(" = ")
+      .append(this.getNaming().getName(node.getLeft()))
+      .append(".equals(")
+      .append(this.getNaming().getName(node.getRight()))
+      .append(")").append(";").append(System.lineSeparator());
+  }
+
+  @Override
+  public void endVisit(ASTLessEqualExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "<=", "isLessThan");
+
+    /* TODO
+    if(OCLHelper.isAmount((node.getLeftExpression()))) {
+      StringBuilder sb = getStringBuilder();
+      OCLVariableNaming varNaming = getVarNaming();
+
+      sb.append(varNaming.getName(node));
+      sb.append(" |= ");
+      sb.append(varNaming.getName(node.getLeftExpression()));
+      sb.append(".approximates(");
+      sb.append(varNaming.getName(node.getRightExpression()));
+      sb.append(");\n");
+    }*/
+  }
+
+  @Override
+  public void endVisit(ASTGreaterEqualExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, ">=", "isGreaterThan");
+
+    /* TODO
+    if(OCLHelper.isAmount((node.getLeftExpression()))) {
+      StringBuilder sb = getStringBuilder();
+      OCLVariableNaming varNaming = getVarNaming();
+
+      sb.append(varNaming.getName(node));
+      sb.append(" |= ");
+      sb.append(varNaming.getName(node.getLeftExpression()));
+      sb.append(".approximates(");
+      sb.append(varNaming.getName(node.getRightExpression()));
+      sb.append(");\n");
+    }*/
+  }
+
+  @Override
+  public void endVisit(ASTLessThanExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, "<", "isLessThan");
+  }
+
+  @Override
+  public void endVisit(ASTGreaterThanExpression node) {
+    Preconditions.checkNotNull(node);
+    this.handleInfixExpression(node, ">=", "isGreaterThan");
+  }
+
+  @Override
+  public void endVisit(ASTEqualsExpression node) {
+    Preconditions.checkNotNull(node);
+    Preconditions.checkNotNull(node);
+    this.getStringBuilder().append("Boolean ")
+      .append(this.getNaming().getName(node))
+      .append(" = ")
+      .append(this.getNaming().getName(node.getLeft()))
+      .append(false ? /*TODO OCLHelper.isAmount((node.getLeft())) */ ".approximates(" : ".equals(")
+      .append(this.getNaming().getName(node.getRight()))
+      .append(")").append(";").append(System.lineSeparator());
+  }
+
+  @Override
+  public void endVisit(ASTNotEqualsExpression node) {
+    Preconditions.checkNotNull(node);
+    this.getStringBuilder().append("Boolean ")
+      .append(this.getNaming().getName(node))
+      .append(" = !")
+      .append(this.getNaming().getName(node.getLeft()))
+      .append(false ? /*TODO OCLHelper.isAmount((node.getLeft())) */ ".approximates(" : ".equals(")
+      .append(this.getNaming().getName(node.getRight()))
+      .append(")").append(";").append(System.lineSeparator());
   }
 
   protected void handleInfixExpression(ASTInfixExpression node, String operator) {
@@ -71,23 +183,8 @@ public class CommonExpressionsPrinter extends AbstractPrinter implements CommonE
     this.getStringBuilder().append(" ").append(operator).append(" ");
     this.getStringBuilder().append(this.getNaming().getName(node.getRight()));
     this.getStringBuilder().append(";\n");
-  }
-
-  @Override
-  public void endVisit(ASTGreaterThanExpression node) {
-    handleInfixExpression(node, ">=", "isGreaterThan");
-
-    /*TODO
-    if(OCLHelper.isAmount((node.getLeftExpression()))) {
-      StringBuilder sb = getStringBuilder();
-      OCLVariableNaming varNaming = getVarNaming();
-
-      sb.append(varNaming.getName(node));
-      sb.append(" |= ");
-      sb.append(varNaming.getName(node.getLeftExpression()));
-      sb.append(".approximates(");
-      sb.append(varNaming.getName(node.getRightExpression()));
-      sb.append(");\n");
-    }*/
+    this.getStringBuilder().append(this.getNaming().getName(node)).append(" &= ");
+    this.getStringBuilder().append(this.getNaming().getName(node));
+    this.getStringBuilder().append(";\n");
   }
 }
