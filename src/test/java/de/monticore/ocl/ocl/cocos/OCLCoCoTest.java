25c2b02a1053a9e762b204b536055afce7158d08
Flo Drux <drux@se-rwth.de>
2022-05-16 15:14:29 +0200

    cleanup OCLCoCoTest.java

--- src/test/java/de/monticore/ocl/ocl/cocos/OCLCoCoTest.java
+++ src/test/java/de/monticore/ocl/ocl/cocos/OCLCoCoTest.java
@@ -16,10 +16,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 public class OCLCoCoTest extends AbstractTest {
 
@@ -39,9 +39,7 @@ public class OCLCoCoTest extends AbstractTest {
   public void acceptsValidModels(String filename) {
     // todo ignoring test container1.ocl which fails due to
     // https://git.rwth-aachen.de/monticore/monticore/-/issues/3141
-    if (filename.equals("container1.ocl")){
-      return;
-    }
+    assumeFalse(filename.equals("container1.ocl"));
 
     // given
     final Optional<ASTOCLCompilationUnit> ast = parse(prefixValidModelsPath(filename), false);
@@ -65,10 +63,10 @@ public class OCLCoCoTest extends AbstractTest {
 
   @ParameterizedTest
   @CsvSource({
-    "src/test/resources/testinput/oclplibrary/list.ocl",
-    "src/test/resources/testinput/oclplibrary/listAndSet.ocl",
-    "src/test/resources/testinput/oclplibrary/set.ocl",
-    "src/test/resources/testinput/oclplibrary/staticQueries.ocl"
+      "src/test/resources/testinput/oclplibrary/list.ocl",
+      "src/test/resources/testinput/oclplibrary/listAndSet.ocl",
+      "src/test/resources/testinput/oclplibrary/set.ocl",
+      "src/test/resources/testinput/oclplibrary/staticQueries.ocl"
   })
   public void shouldAcceptOclpLibrary(final String oclFile) {
     // given
