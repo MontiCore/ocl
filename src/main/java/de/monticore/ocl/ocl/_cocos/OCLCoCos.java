9a8187c3d6dcbbcb2c0b50f10e2cbdeb74fdb5c9
Flo Drux <drux@se-rwth.de>
2022-09-29 14:59:58 +0200

    Add invalid tests for cocos

--- src/main/java/de/monticore/ocl/ocl/_cocos/OCLCoCos.java
+++ src/main/java/de/monticore/ocl/ocl/_cocos/OCLCoCos.java
@@ -5,14 +5,23 @@ package de.monticore.ocl.ocl._cocos;
 import de.monticore.ocl.oclexpressions._cocos.IterateExpressionVariableUsageIsCorrect;
 import de.monticore.ocl.setexpressions._cocos.SetComprehensionHasGenerator;
 import de.monticore.ocl.types.check.OCLDeriver;
+import de.monticore.ocl.types.check.OCLSynthesizer;
 
 public class OCLCoCos {
 
   public static OCLCoCoChecker createChecker() {
-    return createChecker(new OCLDeriver());
+    return createChecker(new OCLDeriver(), new OCLSynthesizer());
   }
 
+  public static OCLCoCoChecker createChecker(OCLSynthesizer synthesizer) {
+    return createChecker(new OCLDeriver(), synthesizer);
+  }
+  
   public static OCLCoCoChecker createChecker(OCLDeriver deriver) {
+    return createChecker(deriver, new OCLSynthesizer());
+  }
+  
+  public static OCLCoCoChecker createChecker(OCLDeriver deriver, OCLSynthesizer synthesizer) {
     OCLCoCoChecker checker = new OCLCoCoChecker();
     checker.addCoCo(new MethSignatureStartsWithLowerCaseLetter());
     checker.addCoCo(new ConstructorNameStartsWithCapitalLetter());
@@ -27,6 +36,7 @@ public class OCLCoCos {
     checker.addCoCo(new SetComprehensionHasGenerator());
     checker.addCoCo(new UnnamedInvariantDoesNotHaveParameters());
     checker.addCoCo(new PreAndPostConditionsAreBooleanType(deriver));
+    checker.addCoCo(new VariableDeclarationOfCorrectType(deriver, synthesizer));
     return checker;
   }
 }
