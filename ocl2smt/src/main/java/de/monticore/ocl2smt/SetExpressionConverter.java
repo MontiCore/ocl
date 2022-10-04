e1eacbdb79aaf03a6a60cbe6851ed0abb887d193
Valdes Voufo <valdes.voufo@rwth-aachen.com>
2022-10-04 09:56:53 +0200

    delete unused functions

--- ocl2smt/src/main/java/de/monticore/ocl2smt/SetExpressionConverter.java
+++ ocl2smt/src/main/java/de/monticore/ocl2smt/SetExpressionConverter.java
@@ -16,37 +16,5 @@ public class SetExpressionConverter {
         this.context = context;
     }
 
-    public Expr<? extends Sort> convert(ASTLiteralExpression node) {
-        ASTLiteral literal = node.getLiteral();
-        if (literal instanceof ASTBooleanLiteral) {
-            return convertBool((ASTBooleanLiteral) literal);
-        } else if (literal instanceof ASTStringLiteral) {
-            return convertString((ASTStringLiteral) literal);
-        } else if (literal instanceof ASTNatLiteral) {
-            return convertNat((ASTNatLiteral) literal);
-        } else if (literal instanceof ASTBasicDoubleLiteral) {
-            return convertDouble((ASTBasicDoubleLiteral) literal);
-        } else {
-            assert false;
-            Log.error("the conversion of expression with the type " + node.getClass().getName() + "in SMT is not totally implemented");
-            return null;
-        }
 
-    }
-
-    protected BoolExpr convertBool(ASTBooleanLiteral node) {
-        return context.mkBool(node.getValue());
-    }
-
-    protected SeqExpr<CharSort> convertString(ASTStringLiteral node) {
-        return context.mkString(node.getValue());
-    }
-
-    protected IntNum convertNat(ASTNatLiteral node) {
-        return context.mkInt(node.getValue());
-    }
-
-    protected FPNum convertDouble(ASTBasicDoubleLiteral node) {
-        return context.mkFP(node.getValue(), context.mkFPSortDouble());
-    }
 }
