59cf8ec8188b22cf474a78ec271fac44ca81e0a3
Felix Jordan <felix.jordan1@rwth-aachen.de>
2025-08-01 17:06:52 +0200

    test: task management restructure

--- ref-ocl-adaptation/src/test/java/de/monticore/oclrefadaptation/EvaluationOCLAdaptationTest.java
+++ ref-ocl-adaptation/src/test/java/de/monticore/oclrefadaptation/EvaluationOCLAdaptationTest.java
@@ -189,13 +189,14 @@ public class EvaluationOCLAdaptationTest extends AbstractOCLAdapterTest {
   class TaskManagement {
 
     @Test
-    void singleInc() {
+    void singleIncBehindSchedule() {
       confParameters.add(CDConfParameter.STRICT_PARAMETER_ORDER);
       testAdaptedEqualsExpected(
               "tasks/singleInc/TaskManagementConc.cd",
               "tasks/TaskManagementRef.cd",
-              "tasks/TaskManagementRef.ocl",
-              "tasks/singleInc/TaskManagementOut.ocl");
+              "tasks/BehindScheduleRef.ocl",
+              "tasks/singleInc/BehindScheduleOut.ocl");
     }
+
   }
 }
