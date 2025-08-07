3a219d838411f042606d093c904010a542c63881
Felix Jordan <felix.jordan1@rwth-aachen.de>
2025-08-07 15:23:01 +0200

    fix: not filter by return type when resolving OCLMethodSignature

--- ref-ocl-adaptation/src/main/java/de/monticore/ocl/OCLAdaptationUtils.java
+++ ref-ocl-adaptation/src/main/java/de/monticore/ocl/OCLAdaptationUtils.java
@@ -5,8 +5,11 @@ import de.monticore.symbols.basicsymbols._symboltable.TypeSymbol;
 import de.monticore.symbols.oosymbols._symboltable.IOOSymbolsScope;
 import de.monticore.symbols.oosymbols._symboltable.MethodSymbol;
 import de.monticore.symboltable.modifiers.AccessModifier;
+import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types3.TypeCheck3;
 import de.se_rwth.commons.logging.Log;
+
+import java.util.Optional;
 
 public class OCLAdaptationUtils {
 
@@ -15,19 +18,16 @@ public class OCLAdaptationUtils {
 
   public static MethodSymbol resolveMethodSymbol(IOOSymbolsScope scope, ASTOCLMethodSignature oclMethodSignature) {
     //  TODO we have to manually resolve the MethodSymbol here the method name here, check parametrs -> respect imports ??
-    TypeSymbol returnTypeSymbol = TypeCheck3.symTypeFromAST(oclMethodSignature.getMCReturnType()).getTypeInfo();
+
     // TODO get type symbols of parameters
 
     // TODO check imports and qualify name if necessary
     String methodName = oclMethodSignature.getMethodName().getQName();
     return scope.resolveMethodDown(methodName, AccessModifier.ALL_INCLUSION, symbol -> {
-            if (!symbol.getType().getTypeInfo().getFullName().equals(returnTypeSymbol.getFullName())) {
-              return false;
-            }
             // TODO check parameter types (or name of we ignore param types. see CDCOnfParameter)
             return true;
           }).orElseGet(() -> {
-            Log.error("0xA1235 Could not resolve method symbol for " + methodName + " in scope "
+            Log.error("Could not resolve method symbol for " + methodName + " in scope "
                     + scope.getName());
             return null;
           });
