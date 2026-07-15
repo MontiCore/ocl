5ead8a6fa0a032bfc6cce6803f15f14a0035bdcd
Alexander Hellwig <hellwig@se-rwth.de>
2026-07-15 16:09:44 +0200

    Typecheck: Fix special cases of type identifiers in OCL

--- src/main/java/de/monticore/ocl/types3/util/OCLNameExpressionTypeCalculator.java
+++ src/main/java/de/monticore/ocl/types3/util/OCLNameExpressionTypeCalculator.java
@@ -7,10 +7,13 @@ import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.mccollectiontypes.types3.util.MCCollectionSymTypeFactory;
 import de.monticore.types3.util.OOWithinScopeBasicSymbolsResolver;
 import de.monticore.types3.util.WithinScopeBasicSymbolsResolver;
+
 import java.util.Optional;
 import java.util.function.Predicate;
 
-/** @deprecated use OCLWithinScopeBasicSymbolResolver */
+/**
+ * @deprecated use OCLWithinScopeBasicSymbolResolver
+ */
 @Deprecated(forRemoval = true)
 public class OCLNameExpressionTypeCalculator extends OOWithinScopeBasicSymbolsResolver {
 
@@ -26,14 +29,20 @@ public class OCLNameExpressionTypeCalculator extends OOWithinScopeBasicSymbolsRe
    */
   @Override
   public Optional<SymTypeExpression> _resolveNameAsExpr(
-      IBasicSymbolsScope enclosingScope, String name) {
+      IBasicSymbolsScope enclosingScope,
+      String name,
+      boolean resultsAreOptional) {
     // case "normal" expression
     Optional<SymTypeExpression> type = super._resolveNameAsExpr(enclosingScope, name);
     // case type id -> create Set of the same type
     if (type.isEmpty()) {
       Optional<SymTypeExpression> typeId = resolveType(enclosingScope, name);
       if (typeId.isPresent()) {
-        type = Optional.of(MCCollectionSymTypeFactory.createSet(typeId.get().deepClone()));
+        if (resultsAreOptional) {
+          type = typeId;
+        } else {
+          type = Optional.of(MCCollectionSymTypeFactory.createSet(typeId.get().deepClone()));
+        }
       }
     }
     return type;
