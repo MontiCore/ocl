c3c72bb5b0eca1c3df46ba0a719d09956bf6c30d
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-07-17 16:20:00 +0200

    enable some tests that have been fixed

--- src/test/java/de/monticore/ocl/ocl/cocos/OCLCoCoTest.java
+++ src/test/java/de/monticore/ocl/ocl/cocos/OCLCoCoTest.java
@@ -1,20 +1,23 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.ocl.ocl.cocos;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
-
 import de.monticore.ocl.ocl.AbstractTest;
 import de.monticore.ocl.ocl._ast.ASTOCLCompilationUnit;
-import de.monticore.ocl.ocl._cocos.*;
+import de.monticore.ocl.ocl._cocos.OCLCoCoChecker;
+import de.monticore.ocl.ocl._cocos.OCLCoCos;
 import de.monticore.ocl.util.SymbolTableUtil;
 import de.se_rwth.commons.logging.Log;
-import java.util.Optional;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 public class OCLCoCoTest extends AbstractTest {
 
@@ -27,53 +30,13 @@ public class OCLCoCoTest extends AbstractTest {
   @ParameterizedTest
   @MethodSource("getValidCocoModels")
   public void acceptsValidModels(String filename) {
+    // inference is kept at a specific complexity
+    assumeFalse(filename.endsWith("comprehension8.ocl"));
     // todo https://git.rwth-aachen.de/monticore/monticore/-/issues/3462
     assumeFalse(filename.contains("cases2"));
     // todo https://git.rwth-aachen.de/monticore/monticore/-/issues/4179
-    assumeFalse(filename.contains("setoperations10"));
     // todo https://git.rwth-aachen.de/monticore/monticore/-/issues/4509
-    Assumptions.assumeFalse(
-        filename.endsWith("Test06.ocl")
-            || filename.endsWith("Test08.ocl")
-            || filename.endsWith("Test01.ocl")
-            || filename.endsWith("Test02.ocl")
-            || filename.endsWith("Test03.ocl")
-            || filename.endsWith("Test04.ocl")
-            || filename.endsWith("Test07.ocl")
-            || filename.endsWith("Test09.ocl")
-            || filename.endsWith("comprehension10.ocl")
-            || filename.endsWith("comprehension12.ocl")
-            || filename.endsWith("comprehension13.ocl")
-            || filename.endsWith("comprehension3.ocl")
-            || filename.endsWith("comprehension5.ocl")
-            || filename.endsWith("comprehension7.ocl")
-            || filename.endsWith("comprehension8.ocl")
-            || filename.endsWith("comprehension9.ocl")
-            || filename.endsWith("invalidVariableDeclaration.ocl")
-            || filename.endsWith("listsAndSetsAsArguments.ocl")
-            || filename.endsWith("prepost12.ocl")
-            || filename.endsWith("prepost13.ocl")
-            || filename.endsWith("quantifiers12.ocl")
-            || filename.endsWith("quantifiers13.ocl")
-            || filename.endsWith("setoperations11.ocl")
-            || filename.endsWith("setoperations13.ocl")
-            || filename.endsWith("setoperations15.ocl")
-            || filename.endsWith("setoperations16.ocl")
-            || filename.endsWith("setoperations6.ocl")
-            || filename.endsWith("setoperations8.ocl")
-            || filename.endsWith("sizeAndLength.ocl")
-            || filename.endsWith("special2.ocl")
-            || filename.endsWith("special3.ocl")
-            || filename.endsWith("special4.ocl")
-            || filename.endsWith("special5.ocl")
-            || filename.endsWith("typeIfString.ocl")
-            || filename.endsWith("typeIf1.ocl")
-            || filename.endsWith("ParameterDeclarationName.ocl")
-            || filename.endsWith("cases1.ocl")
-            || filename.endsWith("association2.ocl")
-            || filename.endsWith("commonOperatorsBoolean.ocl")
-            || filename.endsWith("commonOperatorsInteger.ocl")
-            || filename.endsWith("comparisons.ocl"));
+    // requires further tests
     // given
     final Optional<ASTOCLCompilationUnit> ast = parse(filename, false);
     assertTrue(ast.isPresent());
@@ -102,7 +65,7 @@ public class OCLCoCoTest extends AbstractTest {
     final ASTOCLCompilationUnit ast = optAST.get();
 
     SymbolTableUtil.prepareMill();
-    
+
     SymbolTableUtil.loadSymbolFile("src/test/resources/testinput/CDs/AuctionCD.sym");
     SymbolTableUtil.loadSymbolFile("src/test/resources/testinput/CDs/DefaultTypes.sym");
 
@@ -172,10 +135,10 @@ public class OCLCoCoTest extends AbstractTest {
 
   @ParameterizedTest
   @CsvSource({
-    "src/test/resources/testinput/parsable/symtab/coco/not_javagen/list_oclplibrary.ocl",
-    "src/test/resources/testinput/parsable/symtab/coco/not_javagen/listAndSet.ocl",
-    "src/test/resources/testinput/parsable/symtab/coco/not_javagen/set.ocl",
-    "src/test/resources/testinput/parsable/symtab/coco/not_javagen/staticQueries.ocl"
+      "src/test/resources/testinput/parsable/symtab/coco/not_javagen/list_oclplibrary.ocl",
+      "src/test/resources/testinput/parsable/symtab/coco/not_javagen/listAndSet.ocl",
+      "src/test/resources/testinput/parsable/symtab/coco/not_javagen/set.ocl",
+      "src/test/resources/testinput/parsable/symtab/coco/not_javagen/staticQueries.ocl"
   })
   public void shouldAcceptOclpLibrary(final String oclFile) {
     // given
