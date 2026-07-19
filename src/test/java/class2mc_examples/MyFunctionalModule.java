1a9267c38b80f12be901529161a3d051a2720692
Alexander Hellwig <hellwig@se-rwth.de>
2026-07-19 12:17:46 +0200

    Add minimal failing test case

---
+++ src/test/java/class2mc_examples/MyFunctionalModule.java
@@ -0,0 +1,14 @@
+// (c) https://github.com/MontiCore/monticore
+package class2mc_examples;
+
+import java.util.Collection;
+
+public final class MyFunctionalModule {
+
+  private MyFunctionalModule() {
+  }
+
+  public static long someFunction(Collection<Long> values) {
+    return 0;
+  }
+}
